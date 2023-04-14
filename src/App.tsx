e0520c4eed0a556f6c03a94cd0cc9e5862f475c8
Ali Jahankah <aral2@yahoo.com>
2023-04-14 14:02:00 +0100

    Testing React query devtools in production mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import React, { FC, ReactElement } from 'react';
 import { customTheme } from './theme/customTheme';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import Dashboard from './pages/dashboard/Dashboard';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const App: FC = (): ReactElement => {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline></CssBaseline>
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline></CssBaseline>
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools
+        initialIsOpen={false}
+      ></ReactQueryDevtools>
+    </QueryClientProvider>
   );
 };
 
