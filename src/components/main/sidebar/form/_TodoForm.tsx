1a9107df6940fc2a81616d2c073bfb9cb3470040
Ali Jahankah <aral2@yahoo.com>
2023-04-15 12:24:30 +0100

    using useMutate

--- src/components/main/sidebar/form/_TodoForm.tsx
+++ src/components/main/sidebar/form/_TodoForm.tsx
@@ -4,25 +4,62 @@ import {
   Button,
   Stack,
   Typography,
+  Alert,
+  AlertTitle,
+  LinearProgress,
 } from '@mui/material';
 import React, { FC, ReactElement, useState } from 'react';
 import TextFields from '../inputs/TextFields';
 import DateInput from '../inputs/DateInput';
 import SelectInput from '../inputs/SelectInput';
 import { Status, Level } from '../interfaces/ISelect';
-
+import { useMutation } from '@tanstack/react-query';
+import { ICreateTask } from '../interfaces/IRequests';
+import { sendRequest } from '../../../../helpers/sendApiReqs';
 const TodoForm: FC = (): ReactElement => {
   const [title, setTitle] = useState<null | string>('');
   const [desc, setDesc] = useState<null | string>('');
   const [date, setDate] = useState<Date | null>(new Date());
   const [status, setStatus] = useState<string>(Status.new);
   const [level, setLevel] = useState<string>(Level.easy);
-  console.log({ title, desc, date, status, level });
+
+  const createTaskMutation = useMutation(
+    (data: ICreateTask) =>
+      sendRequest(
+        'http://localhost:4000/tasks/new-task',
+        'Post',
+        data,
+      ),
+  );
+  const createTaskHandler = () => {
+    if (!title || !desc || !date) {
+      return alert('Please fill all the inputs!');
+    }
+    const newTask: ICreateTask = {
+      title,
+      description: desc,
+      date: date.toString(),
+      status,
+      level,
+    };
+    createTaskMutation.mutate(newTask);
+  };
+
   return (
     <Box>
       <Typography variant="h6" m={3}>
         Create a new task
       </Typography>
+      <Alert
+        severity="success"
+        sx={{
+          margin: '1em auto',
+          width: '100%',
+        }}
+      >
+        <AlertTitle>Success!</AlertTitle>
+        New Todo Added!
+      </Alert>
       <Stack spacing={2}>
         <TextFields
           title={title}
@@ -60,6 +97,7 @@ const TodoForm: FC = (): ReactElement => {
             }
           ></SelectInput>
         </Stack>
+        <LinearProgress></LinearProgress>
         <Button variant="contained" size="large" fullWidth>
           Submit
         </Button>
